76636140d29a688cbbc0d9d8493e7d7c9db5cd43
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2022-12-21 16:26:54 +0900

    add greedy bitmasking 2진법 binary

--- BackJoon/Silver1/BOJ1052.cpp
+++ BackJoon/Silver1/BOJ1052.cpp
@@ -3,10 +3,28 @@
 
 using namespace std;
 
+/*
+
+n & (n-1) 연산 의미  
+2진수에서 가장 오른쪽 버리는 연산 
+n = 1100 
+n-1 = 1011
+--> 1000
+
+N&(-N) 연산 의미  
+n = 001  -n =  111   --->    n&-n = 001
+n = 010  -n =  110   --->    n&-n = 010
+n = 100  - n = 100   --->    n&-n = 100
+
+bit가 1인 녀석중 가장 오른쪽을 가져오는 연산
+*/
+
+
+
 int N,K;
 int ans=0;
 
-int BinaryCount(int n){
+int BinaryCount(int n){ // 2진수에서 1의 개수 리턴 
 	int cnt = 0;
 	
 	for(int i = 0 ;  n != 0 ; i++){
@@ -22,12 +40,6 @@ int main(){
 	ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
 	cin>>N>>K;
 
-//	int add = N&(-N);
-//	ans+= add;
-//	N+= add;
-//	cout<<add;
-
-
 	while(1){
 		if (BinaryCount(N)<=K){
 			cout<<ans;
@@ -37,8 +49,6 @@ int main(){
 			int add = N&(-N);
 			ans+= add;
 			N+= add;
-			
-//			cout<<add<<"\n";
 		}
 		
 	}
