cd6db13adf67c1570bc2ff368f717e0cd3c249c5
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2023-03-25 17:36:47 +0900

    add silver2 bfs 내림차순

---
+++ BackJoon/Silver2 이하/BOJ24445.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <queue>
+#include <set>
+
+#define MAX 100010
+using namespace std;
+
+int N,M,R;
+int ans[MAX]={0,};
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	cin>>N>>M>>R;
+	
+	set<int> S[N+1];
+	for(int i = 0,u,v ; i < M ; i++){
+		cin>>u>>v;
+		S[u].insert(v);
+		S[v].insert(u);
+	}
+	
+	
+	queue<int> Q;
+	Q.push(R);
+	int order=1;
+	ans[R]=order++;
+	while(!Q.empty()){
+		int curr = Q.front();
+		Q.pop();
+
+		for(auto it = S[curr].rbegin(); it != S[curr].rend() ; it++){
+			if(!ans[*it]){
+				Q.push(*it);
+				ans[*it]=order++;
+			}
+			
+		}
+		
+	}
+	for(int i = 1 ; i <=N ; i++){
+		cout<<ans[i]<<"\n";
+	}
+	
+}
