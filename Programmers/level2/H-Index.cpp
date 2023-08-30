73e454a3c9e799fdc2416040aaac5b8d9e5641ce
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2023-08-30 11:16:57 +0900

    누적 버전 더 간략하게

--- Programmers/level2/H-Index.cpp
+++ Programmers/level2/H-Index.cpp
@@ -7,7 +7,7 @@ using namespace std;
 /*
 - 문제 조건에서 인용 수 max가 논문 수 보다 큼
 - 인용된 논문 수가 인용 수를 넘어갈때 안용 논문 수 리턴 
-
+        
 */
 int solution(vector<int> citations) {
     sort(citations.begin(),citations.end(),greater<int>());//내림차순
