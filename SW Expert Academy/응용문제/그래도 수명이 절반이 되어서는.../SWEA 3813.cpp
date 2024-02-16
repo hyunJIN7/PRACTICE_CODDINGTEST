8fc6c8777e33e20ddc7e57b9a4dbfea1f4a6ce4b
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2024-02-16 17:40:42 +0900

    Update SWEA 3813.cpp

--- SW Expert Academy/응용문제/그래도 수명이 절반이 되어서는.../SWEA 3813.cpp
+++ SW Expert Academy/응용문제/그래도 수명이 절반이 되어서는.../SWEA 3813.cpp
@@ -54,3 +54,39 @@ int main(int argc, char** argv)
 	}
 	return 0;
 }
+
+
+
+/*
+ // 덩어리들이 차지하는 블록의 최대 Wear Level이 mid이하를 만족하는 경우가 있는지 여부 리턴
+bool isPossible(int x) {
+	int k = 0; // 몇 번째 덩어리인지
+
+	// 블록 순차탐색하면서
+	for (int i = 0; i < N; i++) {
+		// x를 넘으면 안됨
+		if (arr[i] > x) continue;
+
+		// 현재 덩어리의 끝 지점 (i부터 덩어리 크기 만큼)
+		int end = i + brr[k];
+		// 현재 덩어리를 i~end위치에 두었을 때, 덩어리가 차지하는 블록이 x이하를 만족하는지 체크
+		for (int j = i; j < end && j < N; j++) {
+			// x를 넘으면 안됨
+			if (arr[j] > x)  {
+				i = j;
+				break;
+			}
+
+			// 현재 덩어리가 차지하는 블록이 x이하를 만족하는 경우
+			if (j == end - 1) {
+				i = j;
+				k++;
+			}
+		}
+
+		// 모든 덩어리에 대해 x이하인 블록을 차지할 수 있는 경우
+		if (k == K) return true;
+	}
+	return false;
+
+*/
