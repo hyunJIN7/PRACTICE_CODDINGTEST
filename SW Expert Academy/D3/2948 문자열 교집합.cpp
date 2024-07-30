5e34fbe3c8cc8348abdff400ddaa4e6f5a5d4b86
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2024-07-30 11:48:07 +0900

    unordered_map (map으로 하면 시초)

--- SW Expert Academy/D3/2948 문자열 교집합.cpp
+++ SW Expert Academy/D3/2948 문자열 교집합.cpp
@@ -1,34 +1,30 @@
 #include<iostream>
-#include <algorithm>
-#include <vector>
 #include <unordered_map>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-	int test_case;
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	int T;
+	cin >> T;
 
-	cin>>T;
-
-	for(test_case = 1; test_case <= T; ++test_case)
+	for (int test_case = 1; test_case <= T; ++test_case)
 	{
 		unordered_map<string, int> m;
-		int N,M;
+		int N, M;
 		cin >> N >> M;
 		string str;
 		int ans = 0;
-		for(int i = 0 ; i < N ; i++) {
-			cin>>str;
-			m[str]++;	
+		while(N--){
+			cin >> str;
+			m[str]++;
 		}
-		for(int i = 0 ; i < M ; i++) {
-			cin>>str;
-			if(m[str])	 ans++;
+		while(M--){
+			cin >> str;
+			if (m[str])	 ans++;
 		}
-		cout << "#" << test_case <<" "<<ans<<"\n";
+		cout << "#" << test_case << " " << ans << "\n";
 	}
 	return 0;
 }
